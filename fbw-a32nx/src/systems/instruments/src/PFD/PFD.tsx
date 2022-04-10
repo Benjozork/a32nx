32dd97b6766f2ca7b5766e2c46f06ff076c2aa7e
Benjamin Dupont <benjozorkfr@gmail.com>
2022-04-10 02:18:36 -0400

    refactor: prepare for nd-v2

--- fbw-a32nx/src/systems/instruments/src/PFD/PFD.tsx
+++ fbw-a32nx/src/systems/instruments/src/PFD/PFD.tsx
@@ -1,11 +1,11 @@
-import { A320Failure, FailuresConsumer } from '@failures';
+import { A320Failure, FailuresConsumer } from '@flybywiresim/failures';
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { ClockEvents, ComponentProps, DisplayComponent, EventBus, FSComponent, Subject, VNode } from 'msfssdk';
 import { Arinc429Word } from '@shared/arinc429';
 import { DisplayManagementComputerEvents } from 'instruments/src/PFD/shared/DisplayManagementComputer';
 import { LagFilter } from './PFDUtils';
 import { Arinc429Values } from './shared/ArincValueProvider';
-import { DisplayUnit } from './shared/displayUnit';
+import { DisplayUnit } from '../MsfsAvionicsCommon/displayUnit';
 import './style.scss';
 import { AltitudeIndicator, AltitudeIndicatorOfftape } from './AltitudeIndicator';
 import { AttitudeIndicatorFixedCenter, AttitudeIndicatorFixedUpper } from './AttitudeIndicatorFixed';
